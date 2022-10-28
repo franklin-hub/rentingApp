86b5a81ef0e991352ef28a2dc222ad70b310ad3b
brice franklin <bricefongang10@gmail.com>
2022-10-28 07:00:59 +0100

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,29 @@
 import React from "react";
-import {BrowserRouter as Routers, Routes,Route} from 'r'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from './containers/Home'
+import About from './containers/About'
+import Contact from './containers/Contact'
+import Listings from './containers/Listings'
+import ListingDetail from './containers/ListingDetail'
+import LogIn from './containers/LogIn'
+import SignUp from './containers/SignUp'
+import NotFound from './components/NotFound'
+
+import './sass/main.scss'
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/About" element={<About/>} />
+      <Route exact path="/Contact" element={<Contact/>} />
+      <Route exact path="/Listings" element={<Listings/>} />
+      <Route exact path="/Listings/:id" element={<ListingDetail/>} />
+      <Route exact path="/LogIn" element={<LogIn/>} />
+      <Route exact path="/SignUp" element={<SignUp/>} />
+      <Route  path="**" element={<NotFound/>} />
+    </Routes>
+  </Router>
+)
 
 export default App;
